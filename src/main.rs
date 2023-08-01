3e0d79a1d61775aec186342208aafe80d49a6813
Danny Zhu <dzhu@dzhu.us>
2023-07-31 18:34:39 -0600

    Explore minimal partitions and A/B table, add CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
 use std::{
+    cmp::Reverse,
     collections::BTreeMap,
-    env,
     fmt::{Display, Error, Formatter},
 };
+
+use gumdrop::Options;
+use image::RgbImage;
+use imageproc::rect::Rect;
+use rusttype::{Font, Scale};
+
+#[derive(Debug, Options)]
+enum Opts {
+    DrawTable(DrawTableOpts),
+    CountMinimal(CountMinimalOpts),
+    ShowAll(ShowAllOpts),
+}
+
+#[derive(Debug, Options)]
+struct DrawTableOpts {
+    #[options(free)]
+    sz: usize,
+}
+
+#[derive(Debug, Options)]
+struct CountMinimalOpts {
+    #[options(free)]
+    start: usize,
+
+    #[options(free)]
+    end: usize,
+}
+
+#[derive(Debug, Options)]
+struct ShowAllOpts {
+    #[options(free)]
+    sz: usize,
+}
 
 #[derive(Debug)]
 struct Path {
@@ -84,34 +117,250 @@ fn all_paths(sz: usize) -> Vec<Path> {
     ret
 }
 
-fn main() {
-    let args: Vec<_> = env::args().collect();
-    let sz: usize = args[1].parse().unwrap();
+fn tri(n: usize) -> usize {
+    n * (n + 1) / 2
+}
 
-    let mut by_total_and_area = BTreeMap::<usize, BTreeMap<usize, Vec<Path>>>::new();
+fn search_minimal_partitions(sz: usize) -> Vec<Vec<usize>> {
+    fn helper<F: FnMut(&[usize])>(
+        cur: &mut Vec<usize>,
+        cur_total: usize,
+        best_total: &mut usize,
+        remaining: usize,
+        last: usize,
+        last_count: usize,
+        cb: &mut F,
+    ) {
+        if remaining == 0 {
+            *best_total = cur_total.min(*best_total);
+            cb(cur);
+            return;
+        }
+        if cur_total > *best_total {
+            return;
+        }
 
+        if last_count < 2 && last <= remaining {
+            cur.push(last);
+            helper(
+                cur,
+                cur_total + tri(last - 1) + remaining - last,
+                best_total,
+                remaining - last,
+                last,
+                last_count + 1,
+                cb,
+            );
+            cur.pop();
+        }
+        for next in (last.saturating_sub(2).max(1)..last).rev() {
+            if next <= remaining {
+                cur.push(next);
+                helper(
+                    cur,
+                    cur_total + tri(next - 1) + remaining - next,
+                    best_total,
+                    remaining - next,
+                    next,
+                    1,
+                    cb,
+                );
+                cur.pop();
+            }
+        }
+    }
+
+    let mut ret = vec![];
+    let mut best_total = sz * (sz + 1) / 2;
+    for start in (1..=sz).rev() {
+        helper(
+            &mut vec![start],
+            tri(start - 1) + sz - start,
+            &mut best_total,
+            sz - start,
+            start,
+            1,
+            &mut |p| {
+                ret.push(p.to_owned());
+            },
+        );
+    }
+    ret
+}
+
+fn count_minimal_partitions(start: usize, end: usize) {
+    for sz in start..=end {
+        fn value(p: &[usize]) -> usize {
+            let a: usize = p.iter().map(|&n| n * (n - 1) / 2).sum();
+            let b: usize = p.iter().enumerate().map(|(i, &n)| i * n).sum();
+            a + b
+        }
+
+        let ps = search_minimal_partitions(sz);
+        let min = ps.iter().map(|p| value(p)).min().unwrap();
+        println!("================ {sz} {min} {}", ps.len());
+        let mut num_mins = 0;
+        let mut areas: BTreeMap<usize, usize> = BTreeMap::new();
+
+        for p in ps {
+            let v = value(&p);
+            if v != min {
+                continue;
+            }
+            num_mins += 1;
+            let area: usize = p.iter().map(|n| tri(n - 1)).sum();
+            *areas.entry(area).or_default() += 1;
+            if true {
+                println!(
+                    "\x1b[{}m{} {p:?}\x1b[m",
+                    if v == min { "32;1" } else { "" },
+                    v
+                );
+            }
+        }
+        println!("{num_mins} mins");
+        for (area, num) in areas {
+            println!("{area} {num}");
+        }
+    }
+}
+
+fn draw_ab_counts(sz: usize) -> RgbImage {
+    const BOX_SEP: usize = 22;
+
+    let mut by_area_and_bounce = BTreeMap::<usize, BTreeMap<usize, usize>>::new();
     for p in all_paths(sz).into_iter() {
         let a = p.area();
         let b = p.bounce();
-        by_total_and_area
-            .entry(a + b)
-            .or_default()
+        *by_area_and_bounce
             .entry(a)
             .or_default()
-            .push(p);
+            .entry(b)
+            .or_default() += 1;
     }
 
-    for (total, by_area) in by_total_and_area {
-        println!("\x1b[32;1m================================================================ total: {total:2}\x1b[m");
-        for (area, paths) in by_area.into_iter().rev() {
-            println!(
-                "\x1b[36m================ area: {area:2}  bounce: {:2}  ({:2} paths)\x1b[m",
-                paths[0].bounce(),
-                paths.len()
-            );
-            for p in paths {
-                println!("bounces: {:?}", p.bounce_locs());
-                println!("{p}");
+    let max = tri(sz - 1);
+    let img_dim = (BOX_SEP * (max + 1) + 1) as u32;
+
+    let mut img = RgbImage::new(img_dim, img_dim);
+
+    let font_data: &[u8] = include_bytes!("/usr/share/fonts/TTF/Anonymous Pro.ttf");
+    let font: Font<'static> = Font::try_from_bytes(font_data).unwrap();
+
+    use imageproc::drawing;
+    let line_color = [64, 64, 64].into();
+    let text_color = [255, 255, 255].into();
+    let chain_color = [0, 40, 0].into();
+
+    let mut min_locs = vec![];
+    for area in 0..=max {
+        for bounce in 0..=max {
+            if let Some(n) = by_area_and_bounce.get(&area).and_then(|m| m.get(&bounce)) {
+                let s = format!("{n}");
+                let scale = Scale::uniform(10.0);
+                let text_size = drawing::text_size(scale, &font, &s);
+                let is_chain_start = area == 0
+                    || by_area_and_bounce
+                        .get(&(area - 1))
+                        .and_then(|m| m.get(&(bounce + 1)))
+                        .unwrap_or(&0)
+                        != n
+                    || bounce == 0
+                    || by_area_and_bounce
+                        .get(&(area + 1))
+                        .and_then(|m| m.get(&(bounce - 1)))
+                        .unwrap_or(&0)
+                        != n;
+
+                if is_chain_start {
+                    drawing::draw_filled_rect_mut(
+                        &mut img,
+                        Rect::at((BOX_SEP * area) as _, (BOX_SEP * bounce) as _)
+                            .of_size(BOX_SEP as _, BOX_SEP as _),
+                        chain_color,
+                    );
+                }
+                drawing::draw_text_mut(
+                    &mut img,
+                    text_color,
+                    (BOX_SEP * area + BOX_SEP / 2) as i32 - text_size.0 / 2,
+                    (BOX_SEP * bounce + BOX_SEP / 2) as i32 - text_size.1 / 2,
+                    scale,
+                    &font,
+                    &s,
+                );
+
+                if area == 0
+                    || by_area_and_bounce
+                        .get(&(area - 1))
+                        .and_then(|m| m.get(&bounce))
+                        .is_none()
+                    || bounce == 0
+                    || by_area_and_bounce
+                        .get(&area)
+                        .and_then(|m| m.get(&(bounce - 1)))
+                        .is_none()
+                {
+                    min_locs.push((area, bounce));
+                }
+            }
+        }
+    }
+
+    for i in 0..=max + 1 {
+        let p0 = ((BOX_SEP * i) as f32, 0.0);
+        let p1 = ((BOX_SEP * i) as f32, (BOX_SEP * (max + 2 - i)) as f32);
+        drawing::draw_line_segment_mut(&mut img, p0, p1, line_color);
+        drawing::draw_line_segment_mut(&mut img, (p0.1, p0.0), (p1.1, p1.0), line_color);
+    }
+
+    min_locs.sort_by_key(|&(a, b)| (a, Reverse(b)));
+    for (a, b) in min_locs {
+        println!("{a} {b}");
+    }
+
+    img
+}
+
+fn main() {
+    let opts = Opts::parse_args_default_or_exit();
+
+    match opts {
+        Opts::DrawTable(DrawTableOpts { sz }) => {
+            let img = draw_ab_counts(sz);
+            img.save("/tmp/peaks.png").unwrap();
+            img.save(format!("/tmp/peaks{sz:02}.png")).unwrap();
+        }
+        Opts::CountMinimal(CountMinimalOpts { start, end }) => {
+            count_minimal_partitions(start, end);
+        }
+        Opts::ShowAll(ShowAllOpts { sz }) => {
+            let mut by_total_and_area = BTreeMap::<usize, BTreeMap<usize, Vec<Path>>>::new();
+
+            for p in all_paths(sz).into_iter() {
+                let a = p.area();
+                let b = p.bounce();
+                by_total_and_area
+                    .entry(a + b)
+                    .or_default()
+                    .entry(a)
+                    .or_default()
+                    .push(p);
+            }
+
+            for (total, by_area) in by_total_and_area {
+                println!("\x1b[32;1m================================================================ total: {total:2}\x1b[m");
+                for (area, paths) in by_area.into_iter().rev() {
+                    println!(
+                        "\x1b[36m================ area: {area:2}  bounce: {:2}  ({:2} paths)\x1b[m",
+                        paths[0].bounce(),
+                        paths.len()
+                    );
+                    for p in paths {
+                        println!("bounces: {:?}", p.bounce_locs());
+                        println!("{p}");
+                    }
+                }
             }
         }
     }
